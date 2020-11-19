d41cf545fb7878e1bda1c470265513c21a9e25b4
Elijah Atkins <elijahorama@gmail.com>
2020-11-18 20:37:46 -0700

    day4 homework

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,14 +10,14 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+#map_file = "maps/test_line.txt"
+#map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,34 +29,71 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+opposite_direction = {
+    's': 'n',
+    'n': 's',
+    'e': 'w',
+    'w': 'e'
+}
+
+reverse_path = [None]
+
+visited = {}
+
+while len(visited) < len(room_graph) - 1:
+
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = player.current_room.get_exits()
+        print("ADD ROOM TO VISITED", visited)
+        print(reverse_path)
+        if reverse_path[-1]:
+            print("REVERSE PATH BEFORE", reverse_path)
+            visited[player.current_room.id].remove(reverse_path[-1])
+
+            print("VISITED AFTER REMOVING PREV DIRECTION", visited)
+
+        else:
+            continue
+
+    while len(visited[player.current_room.id]) == 0:
+        previous_path = reverse_path.pop()
+        traversal_path.append(previous_path)
+        player.travel(previous_path)
+
+    next_direction = visited[player.current_room.id].pop()
+    traversal_path.append(next_direction)
+    reverse_path.append(opposite_direction[next_direction])
+    print("REVERSE PATH", reverse_path)
+    player.travel(next_direction)
+    print("TRAVERSAL PATH", traversal_path)
 
 
 # TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
+# visited_rooms = set()
+# player.current_room = world.starting_room
+# visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
 
-if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
+# if len(visited_rooms) == len(room_graph):
+#     print(
+#         f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+# else:
+#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+#     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
